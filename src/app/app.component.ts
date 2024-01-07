e15781b02617c0cf8c4fec2884cb9bc9aea6bd69
HanhDHT <doanhonghanh.1999@gmail.com>
2024-01-08 02:07:54 +0700

    mini project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { AuthModule } from "./auth/auth.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HomeComponent } from "./pages/home/home.component";
+import { BooksModule } from "./books/books/books.module";
+import { HttpClientModule } from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet, 
+    AuthModule,
+    BooksModule,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
